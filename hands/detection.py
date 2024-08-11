07b1f9f2a2fb05142f39057ae98b68441f574086
HAMSASHREE SRIVENKATESWARAN <115524863+shree1967@users.noreply.github.com>
2024-08-11 16:43:51 +0530

    detection.py

--- hands/detection.py
+++ hands/detection.py
@@ -4,9 +4,8 @@ from keras.models import load_model
 
 def preprocess_image(image, target_size):
     image = cv2.resize(image, target_size)
-    image = image / 255.0  
+    image = image / 255.0  # Normalize the image
     return image
-
 
 def detect_hand_state(model, image_path, input_shape):
     # Load and preprocess the image
@@ -22,16 +21,15 @@ def detect_hand_state(model, image_path, input_shape):
     
     # Map prediction to label
     if predicted_class == 0:
-        return "Open hand"
-    elif predicted_class == 1:
         return "Closed hand"
+    elif predicted_class == 1:
+        return "Open hand"
     else:
         return "Hand movement not detected"
 
-
 if __name__ == "__main__":
     model_path = 'hand_detection_model.h5'  
-    image_path = 'testimg1.jpg'  
+    image_path = 'testimg1.jpg'  # Replace with your image path
     input_shape = (128, 128, 3)  
     
     # Load the trained model
@@ -40,4 +38,5 @@ if __name__ == "__main__":
     # Detect hand state
     result = detect_hand_state(model, image_path, input_shape)
     print(f"Detected: {result}")
+
 
